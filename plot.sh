a737dd50ec56df84ae5cfeb1a7e6bd0ecd08c435
Adrian Pistol <adinator1999@gmail.com>
2015-11-10 22:08:12 +0100

    Fancier graphing!

--- plot.sh
+++ plot.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env Rscript
 
+require(PerformanceAnalytics)
+
 results <- read.csv("results.csv", header=TRUE, sep=",", as.is=TRUE)
 
+# Order the results, make it look fancy.
+ordered <- results[order(results$Iterations),]
+rordered <- apply(ordered, 2, rev)
+
 pdf("results.pdf")
-barplot(results$Iterations, names.arg=results$Language, horiz=TRUE, las=1, cex.names=0.5, border=NA)
-title("benchmark_loop")
+
+# Bar Plot, speed representation
+barplot(ordered$Iterations, names.arg=ordered$Language, horiz=TRUE, las=1, cex.names=0.5, border=NA)
+title("Visual Comparison")
+
+# Table, contains versions and stuffs too.
+#PerformanceAnalytics:::textplot(results, halign="center", valign="top", row.valign="center")
+PerformanceAnalytics:::textplot(rordered, halign = "center", valign = "top", row.valign="center", mar = c(0,0,3,0)+0.1)
+

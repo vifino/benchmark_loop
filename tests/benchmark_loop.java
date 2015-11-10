66119df96e43e9dbeb0e2f7aae35b9f305fc2b26
Pangea <pangeacake@users.noreply.github.com>
2015-11-09 19:25:47 -0800

    oops

--- tests/benchmark_loop.java
+++ tests/benchmark_loop.java
@@ -1,10 +1,10 @@
 class BenchmarkLoop {
 	public static void main(String[] args) {
 		long iter = 0;
-		start = System.currentTimeMillis();
+		long start = System.currentTimeMillis();
 		while (iter % 128 != 0 || (System.currentTimeMillis() - start) < 1000) {
 			iter++;
 		}
-		System.out.println(iter + " interations");
+		System.out.println(iter + " iterations");
 	}
 }

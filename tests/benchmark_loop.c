278552e5b1bb3cbe00095f014fa8fbc939e923ce
Dwayne Slater <ds84182@gmail.com>
2015-11-10 11:03:56 -0500

    Optimize C Benchmark Loop.
    I still think Java is full of shit.

--- tests/benchmark_loop.c
+++ tests/benchmark_loop.c
@@ -1,16 +1,10 @@
 #include <time.h>
-#include <sys/resource.h>
 #include <stdio.h>
-
-int get_time() {
-	struct timespec t;
-	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &t);
-	return t.tv_sec;
-}
 
 int main() {
 	unsigned long long iter = 0;
-	while (iter % 128 != 0 || get_time() < 1) {
+	struct timespec t;
+	while (iter % 128 != 0 || (clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &t), t.tv_sec) < 1) {
 		iter++;
 	}
 	printf("%llu iterations\n", iter);
